625e83d95f27cd65c091be2814225e8f73ef935d
Tanmay <tanmaylaud@gmail.com>
2020-05-11 22:07:08 +0530

    VPS

--- May/Valid Perfect Square/Solution.java
+++ May/Valid Perfect Square/Solution.java
@@ -1,3 +1,22 @@
-public class Solution {
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num < 2)
+            return true;
+        int L = 2;
+        int R = num;
+        long square;
+        int mid;
+        while (L <= R) {
+            mid = (L + R) / 2;
+            square = (long) mid * mid;
+            if ((int) square == num)
+                return true;
+            else if (square > num) {
+                R = mid - 1;
+            } else
+                L = mid + 1;
+        }
+        return false;
+    }
 
 }
